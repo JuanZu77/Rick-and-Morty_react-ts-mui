4786ab5320d07527adf22f9988be41c635120315
JuanZu77 <juanvzubiri77@gmail.com>
2023-12-20 22:30:45 -0300

    commit theme.config

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Button, Container } from '@mui/material';
+import './App.css'
+
+function App() {
+
+
+  return (
+        <Container maxWidth='xl'>
+
+          <Button variant='contained'>Hola Mundo</Button>
+        </Container>
+    
+  );
+}
+
+export default App
