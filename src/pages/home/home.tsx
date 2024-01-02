bb7b19293e438769de39331e1a41949879046b3b
JuanZu77 <juanvzubiri77@gmail.com>
2024-01-01 21:52:38 -0300

    connect endpoint rick&morty

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,19 @@
 import { Button, Container } from "@mui/material"
 import { HeaderComponent } from "../../components/indexComponents"
+import React from "react"
+import { characters } from "../../api/characters"
 
 
 export const HomePage:React.FC = ()=>{
+
+  React.useEffect(()=>{
+     characters.getAll({page:1}).then((res)=>{
+         console.log(res.data /*.results*/)
+     }).catch((error:Error)=>{
+         console.log(error)
+     })
+  }, []);
+
 
     return(
 
